7f8b1ec11dc4d54415d4e4fdb2553014b3b3c684
mazher123 <mazherulislam6@gmail.com>
2020-01-23 00:46:13 +0600

    file structure ready and component placed

---
+++ resources/js/components/TodoMainPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+               <input type="text" autofocus="autofocus" 
+               
+                placeholder="What are you trying to get done?" class="new-todo">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+
+
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style>
+
+.new-todo {
+     width: 100%;
+    padding: 16px 16px 16px 60px;
+    border: none;
+    background: rgba(0, 0, 0, 0.003);
+    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+}
+
+
+
+</style>
---
+++ resources/views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <Section id="app" class="container">
+         
+         
+            <div class=" mt-5 ml-5">
+             
+                 <todo-main-component></todo-main-component>
+            </div>
+        </section>
+        <script src='js/app.js'></script>
+    </body>
+</html>
