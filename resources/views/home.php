0a1c093490d4cdc05b649c185d8d7053c6dd60c0
mazher123 <mazherulislam6@gmail.com>
2020-01-24 00:04:57 +0600

    identation fixes

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 
-    <head>
-        <link href="css/app.css" rel="stylesheet">
-    </head>
-    <body>
-        <Section id="app" class="container">
-         
-         
-            <div class=" mt-5 ml-5">
-             
-                 <todo-main-component></todo-main-component>
-            </div>
-        </section>
-        <script src='js/app.js'></script>
-    </body>
+<head>
+    <link href="css/app.css" rel="stylesheet">
+</head>
+
+<body>
+    <Section id="app" class="container">
+
+
+        <div class=" mt-5 ml-5">
+
+            <todo-main-component></todo-main-component>
+        </div>
+    </Section>
+    <script src='js/app.js'></script>
+</body>
+
 </html>
